9173d6fb3fbad525988963d1c3d62f9e945f0ad2
Amanda <amanda.steinhauer@bell.net>
2022-03-02 08:06:06 -0500

    update nav bar to remove testimonials

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -14,6 +14,7 @@ class Header extends Component {
     }
 
     // <li><a className="smoothscroll" href="#portfolio">Works</a></li> 
+    //<li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
     return (
       <header id="home">
 
@@ -27,7 +28,7 @@ class Header extends Component {
             <li><a className="smoothscroll" href="#about">About</a></li>
 	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
             
-            <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
+            
             <li><a className="smoothscroll" href="#contact">Contact</a></li>
          </ul>
 
